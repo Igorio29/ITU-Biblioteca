78bdb4b5dcb8770b90fef6b774c7f51bfb5fa01d
Igorio29 <igorjosefarias3@gmail.com>
2024-11-18 22:09:29 -0300

    get funcionando

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,37 @@ app.post("/api/v1/books", async (req, res) => {
         await book.save()
         res.status(201).json({success: true, data: book})
     } catch (error) {
-        console.log("Ocorreu um erro", error)
+        console.error("Ocorreu um erro", error)
         res.status(500).json({success: false, error: "Error saving Book"})
+    }
+})
+
+app.get("/api/v1/books", async (req, res) =>  {
+    try {
+        const books = await Book.find();
+        res.status(200).json({success: true, data: books})
+    } catch (error) {
+        console.error("Ocorreu um erro", error)
+        res.status(500).json({success: false, error: "Erro ao buscar livro"})
+    }
+})
+
+app.get("/api/v1/books/:id", async(req, res) => {
+    const {id} = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({success: false, error: "Id invalido"})
+    }
+
+    try {
+        const book = await Book.findById(id)
+        if(!book){
+            return res.status(404).json({success: false, error: "Livro não encontrado"})
+        } 
+        res.status(300).json({success: true, data: book})
+    } catch (error) {
+        console.error("Error ao procurar o livro", error)
+        res.status(500).json({success: false, error: "Error ao encontrar o livro"})
     }
 })
 
